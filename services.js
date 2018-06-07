b20087bf5baf897103c611b14da8306445a5535e
Mentalstack123 <Mentalstack123@DESKTOP-RRRKS4R>
2018-06-07 11:47:00 +0300

    1

---
+++ services.js
@@ -0,0 +1,5 @@
+﻿angular.module("exampleApp.Services", [])
+        .service("days", function (nowValue) {
+            this.today = nowValue.getDay();
+            this.tomorrow = this.today + 1;
+        });
