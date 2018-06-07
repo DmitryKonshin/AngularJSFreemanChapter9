98cfa4f6e3fa53973317dbcbae25d9eb769368c4
Mentalstack123 <Mentalstack123@DESKTOP-RRRKS4R>
2018-06-07 11:33:32 +0300

    Using Modules to Organize Code

---
+++ filters.js
@@ -0,0 +1,21 @@
+﻿angular.module("exampleApp.Filters", []).filter("dayName", function () {
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    return function (input) {
+        var day = getNameOfDay(input);
+
+        return day == "Thursday" ? "Thursday" : "not Thursday";
+    }
+
+    function getNameOfDay(input) {
+        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = "unknown";
+
+        if (angular.isNumber(input)) {
+            day = dayNames[input];
+        }
+
+        return day;
+    }
+});
